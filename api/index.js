0d484ad27037cb8588b4099de002a379dfbb6f0e
Guy Levi <robozevel@gmail.com>
2020-01-22 03:09:02 +0200

    implemented using got and cheerio

---
+++ api/index.js
@@ -0,0 +1,38 @@
+const { run } = require('micro')
+const cors = require('micro-cors')()
+const { parse } = require('url')
+const cheerio = require('cheerio')
+const got = require('got')
+
+const client = got.extend({
+  prefixUrl: 'https://www.haaretz.co.il/misc/article-print-page/',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
+  }
+})
+
+const style = [
+  'max-width: 64rem',
+  'margin: 0 auto',
+  'font-family: sans-serif',
+  'line-height: 1.6',
+  'padding: 2rem'
+].join(';')
+
+const handler = async (req, res) => {
+  const { pathname } = parse(req.query.url)
+  const article = pathname.split('/').pop()
+  const { body } = await client.get(article)
+  const $ = cheerio.load(body)
+  const teaser = $('.teaser')
+  const p = $('p')
+
+  return `
+    <body dir="rtl" style="${style}">
+      ${teaser}
+      ${p}
+    </body>
+  `
+}
+
+module.exports = cors((req, res) => run(req, res, handler))
