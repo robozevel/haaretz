9659a96a3aebd85c0b0ec287021e0a8c8d50239e
Guy Levi <robozevel@gmail.com>
2020-10-26 04:14:26 +0200

    use amp, css

--- api/index.js
+++ api/index.js
@@ -1,39 +1,69 @@
 const { run } = require('micro')
 const cors = require('micro-cors')()
-const { parse } = require('url')
-const cheerio = require('cheerio')
+const { URL } = require('url')
+const $ = require('cheerio')
 const got = require('got')
 
 const client = got.extend({
-  prefixUrl: 'https://www.haaretz.co.il/misc/article-print-page/',
+  prefixUrl: 'https://www.haaretz.co.il/amp/',
   headers: {
     'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
   }
 })
 
-const style = [
-  'max-width: 64rem',
-  'margin: 0 auto',
-  'font-family: sans-serif',
-  'line-height: 1.6',
-  'padding: 2rem'
-].join(';')
+const css = `
+  body {
+    max-width: 32rem;
+    margin: 2rem auto;
+    font-family: 'Rubik', sans-serif;
+    font-size: 16px;
+    line-height: 1.6;
+    padding: 2rem;
+  }
+
+  figure {
+    margin: 1rem 0;
+    padding: 0 2rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100vw;
+  }
+`
 
 const handler = async (req, res) => {
-  const { pathname } = parse(req.query.url)
-  const article = pathname.split('/').pop()
-  const { body } = await client.get(article)
-  const $ = cheerio.load(body)
-  const teaser = $('.teaser')
-  const p = $('p')
+  const { pathname } = new URL(req.query.url)
+  const path = pathname.split('/').pop()
+  const { body } = await client.get(path)
+  const title = $('title', body).text()
+  const content = $('main article.article', body).find('h1, p.t-body-text, figure.pic')
+
+  content.find('img.lazyload').replaceWith((_, el) => {
+    const img = $(el)
+    const { src, srcset } = img.data()
+    return img.attr({ src, srcset, loading: 'lazy' })
+  })
 
   return `
-    <html dir="rtl">
-      <body style="${style}">
-        ${teaser}
-        ${p}
-      </body>
-    </html>
+  <!DOCTYPE html>
+  <html dir="rtl">
+  <head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${title}</title>
+    <style>
+      ${css}
+    </style>
+  </head>
+  <body>
+    ${content}
+  </body>
+  </html>
   `
 }
 
